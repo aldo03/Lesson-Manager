5a759b4fd1651cabeda8f379a73dbb9a456b89b7
filoberlini <filo.berlini@gmail.com>
2015-05-05 17:06:39 +0200

    add frequency in Lesson

--- app/src/main/java/com/example/matteoaldini/lessonmanager/Lesson.java
+++ app/src/main/java/com/example/matteoaldini/lessonmanager/Lesson.java
@@ -11,14 +11,16 @@ public class Lesson {
     private CalendarDay date;
     private MyTime timeStart;
     private Integer duration;
+    private int frequency;
     private boolean present;
     private boolean paid;
 
-    public Lesson(CalendarDay date, Student student, Integer hour, Integer minute, Integer duration) {
+    public Lesson(CalendarDay date, Student student, Integer hour, Integer minute, Integer duration, int frequency) {
         this.date = date;
         this.student = student;
         this.timeStart = new MyTime(hour, minute);
         this.duration = duration;
+        this.frequency = frequency;
     }
 
     public void setPaid(boolean paid) {
@@ -27,6 +29,26 @@ public class Lesson {
 
     public void setPresent(boolean present) {
         this.present = present;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setDate(CalendarDay date) {
+        this.date = date;
+    }
+
+    public void setTimeStart(MyTime timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
     }
 
     public Student getStudent() {
@@ -47,6 +69,16 @@ public class Lesson {
 
     public Integer getDuration() {
         return duration;
+    }
+
+    /* 0 if is only one lesson
+     * 1 each week
+     * 2 every two weeks
+     * 3 every three weeks
+     * 4..
+     */
+    public int getFrequency() {
+        return frequency;
     }
 
     public boolean isPresent() {
