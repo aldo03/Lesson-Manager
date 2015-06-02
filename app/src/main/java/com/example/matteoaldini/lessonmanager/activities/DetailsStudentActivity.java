00b998a8bda15d27c397ff3a06fc9f2cd0157361
ortbrando <brando.mordenti@gmail.com>
2015-06-03 00:52:03 +0200

    new button lesson ADD

--- app/src/main/java/com/example/matteoaldini/lessonmanager/activities/DetailsStudentActivity.java
+++ app/src/main/java/com/example/matteoaldini/lessonmanager/activities/DetailsStudentActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
